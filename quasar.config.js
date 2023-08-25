0ca3e0d94210e25e923b50d674189c9fc52cfbea
NanPNS <Panisa7766@gmail.com>
2023-08-25 16:18:46 +0700

    'modify' 'notify' in quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -98,7 +98,11 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+
+
+      ]
     },
 
     // animations: 'all', // --- includes all animations
